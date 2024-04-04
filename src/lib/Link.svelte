b70510fbf511d753f2e791ed5cf69f545e964b27
Alvin Philips <alvinphilips257@gmail.com>
2024-04-04 03:17:01 -0400

    Added initial web stuff

---
+++ src/lib/Link.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let address = 'https://youtu.be/dQw4w9WgXcQ';
+    export let tclass = "";
+</script>
+
+<a href={address} class="text-sky-500 hover:text-sky-300 {tclass}">
+<slot />
+</a>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Link from "$lib/Link.svelte";
+</script>
+
+<div class="h-screen w-full flex items-center justify-center bg-emerald-800 flex-col">
+  <div class="w-full lg:w-1/2 bg-white lg:rounded-md flex py-4 flex-col text-center shadow-lg">
+    <h2 class="text-4xl w-full">Forest Cleanup 2024</h2>
+    <h3 class="mt-4 text-xl">April 10th</h3>
+    <h3 class="text-2xl font-mono">2PM - 7PM, multiple batches</h3>
+    <p class="px-12 self-center mt-6">Help us clean the McCraney Valley Trail &lpar;Trafalgar Campus trail&rpar; or just join for a little walk on our lovely trails.</p>
+    <h4 class="text-xl mt-4">Meet @ Sheridan 'S' &lpar;SCAET wing&rpar;</h4>
+    <p class="mt-4">Join our <Link tclass="font-bold" address="https://discord.gg/Bg8jBZm5X9">Discord</Link> to stay updated!</p>
+  </div>
+  <p class="text-white mt-12 bg-emerald-950 p-4 md:rounded-md w-full md:w-auto text-center">Brought to you by <Link address="https://sheridancollege.campuslabs.ca/engage/organization/4thorder">The 4th Order of Shenaniganery</Link> and <Link address="https://sheridancollege.campuslabs.ca/engage/organization/cheese">Sheridan Cheese Club</Link>.</p>
+</div>
